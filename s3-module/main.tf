6ce5a87660380d12740c045f7af9d689639290b9
atemlefeh <doremefasod@gmail.com>
2023-03-18 18:26:10 -0500

    new s3 commit

---
+++ s3-module/main.tf
@@ -0,0 +1,177 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket = "example-private-bucket"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "alias/aws/s3"
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+  lifecycle {
+    rule {
+      id      = "example-rule"
+      status  = "Enabled"
+      prefix  = ""
+      tags    = {}
+      transition {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      }
+      expiration {
+        days = 60
+      }
+    }
+  }
+  object_lock_configuration {
+    object_lock_enabled = "Enabled"
+    rule {
+      default_retention {
+        mode = "GOVERNANCE"
+        days = 30
+      }
+    }
+  }
+  backend {
+    type = "s3"
+    bucket = "example-terraform-state-bucket"
+    key    = "example-private-bucket/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+#################################################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+###########################
+# Customer managed KMS key
+###########################
+resource "aws_kms_key" "kms_s3_key" {
+    description             = "Key to protect S3 objects"
+    key_usage               = "ENCRYPT_DECRYPT"
+    deletion_window_in_days = 7
+    is_enabled              = true
+}
+
+resource "aws_kms_alias" "kms_s3_key_alias" {
+    name          = "alias/s3-key"
+    target_key_id = aws_kms_key.kms_s3_key.key_id
+}
+
+########################
+# Bucket creation
+########################
+resource "aws_s3_bucket" "my_protected_bucket" {
+  bucket = var.bucket_name
+}
+
+##########################
+# Bucket private access
+##########################
+resource "aws_s3_bucket_acl" "my_protected_bucket_acl" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  acl    = "private"
+}
+
+#############################
+# Enable bucket versioning
+#############################
+resource "aws_s3_bucket_versioning" "my_protected_bucket_versioning" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+#################################
+# Enable server access logging
+#################################
+resource "aws_s3_bucket_logging" "my_protected_bucket_logging" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  target_bucket = var.access_logging_bucket_name
+  target_prefix = "${var.bucket_name}/"
+}
+
+##########################################
+# Enable default Server Side Encryption
+##########################################
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_protected_bucket_server_side_encryption" {
+  bucket = aws_s3_bucket.my_protected_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_s3_key.arn
+        sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+############################
+# Creating Lifecycle Rule
+############################
+resource "aws_s3_bucket_lifecycle_configuration" "my_protected_bucket_lifecycle_rule" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.my_protected_bucket_versioning]
+
+  bucket = aws_s3_bucket.my_protected_bucket.bucket
+
+  rule {
+    id = "basic_config"
+    status = "Enabled"
+
+    filter {
+      prefix = "config/"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+    
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "my_protected_bucket_access" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
