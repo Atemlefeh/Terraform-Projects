c574321c9107495c67bc556d8355761c17165ecc
Atem <118619145+Atemlefeh@users.noreply.github.com>
2023-03-28 20:05:31 -0500

    Update main.tf

--- s3-module/main.tf
+++ s3-module/main.tf
@@ -16,52 +16,22 @@ provider "aws" {
   region  = var.aws_region
 }
 
-# Customer managed KMS key
-###########################
-resource "aws_kms_key" "kms_s3_key" {
-    description             = "Key to protect S3 objects"
-    key_usage               = "ENCRYPT_DECRYPT"
-    deletion_window_in_days = 7
-    is_enabled              = true
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
 
-resource "aws_kms_alias" "kms_s3_key_alias" {
-    name          = "alias/s3-key"
-    target_key_id = aws_kms_key.kms_s3_key.key_id
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
 }
 
-# Bucket creation
-########################
-resource "aws_s3_bucket" "ajc_bucket" {
-  bucket = var.bucket_name
-}
-
-# Bucket private access
-##########################
-resource "aws_s3_bucket_acl" "ajc_bucket_acl" {
-  bucket = aws_s3_bucket.ajc_bucket.id
-  acl    = "private"
-}
-
-# Enable bucket versioning
-#############################
-resource "aws_s3_bucket_versioning" "ajc_bucket_versioning" {
-  bucket = aws_s3_bucket.ajc_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Enable default Server Side Encryption
-##########################################
-resource "aws_s3_bucket_server_side_encryption_configuration" "ajc_bucket_sse" {
-  bucket = aws_s3_bucket.ajc_bucket.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.kms_s3_key.arn
-        sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
-
